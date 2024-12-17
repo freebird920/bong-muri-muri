87c092813f2e1bae5585a3ff3b9f98c2afd38818
freebird920 <freebird_han@naver.com>
2024-12-17 21:19:17 +0900

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import { memo } from "react";
 
 const App = memo(() => {
-  return <></>;
+  return (
+    <>
+      <h1 className="text-center text-xl font-extrabold">Home Index</h1>
+    </>
+  );
 });
 App.displayName = "App";
 export default App;
