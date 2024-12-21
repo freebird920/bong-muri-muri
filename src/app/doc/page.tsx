280a8be1e4573805fe4af12c63b39c47133b2a46
freebird920 <freebird_han@naver.com>
2024-12-21 18:12:07 +0900

    update

--- src/app/doc/page.tsx
+++ src/app/doc/page.tsx
@@ -1,10 +1,55 @@
-import { memo } from "react";
+import { memo, useId } from "react";
 
 const DocPage = memo(() => {
+  const compId = useId();
+  const sigmoidFunction = (x: number): number => {
+    return 1 / (1 + Math.exp(-x));
+  };
+  const inputValue: [number, number] = [1, 1];
+  const modelWeight: [number, number] = [1, 1];
+  const doPerceptron = () => {
+    let temp: number = 0;
+    for (const [index, weight] of modelWeight.entries()) {
+      console.log(`Index: ${index}, Weight: ${weight}`);
+      const val = weight * inputValue[index];
+      temp = temp + val;
+    }
+    const bias = -1.5;
+    console.log(temp + bias);
+    return sigmoidFunction(temp + bias);
+  };
   return (
     <div>
       <article>
         <h1>퍼셉트론</h1>
+        <h1>퍼셉트론이란?</h1>
+        <h2>활성함수 - Sigmoid 함수</h2>
+        <pre>
+          {`
+            // typescript
+            const sigmoidFunction = (x: number): number => {
+              return 1 / (1 + Math.exp(-x));
+            };
+          `}
+        </pre>
+        <h2>퍼셉트론으로 AND 논리 회로 구현</h2>
+        <button
+          onClick={() => {
+            const result = doPerceptron();
+            console.log(result);
+          }}
+        >
+          테스트
+        </button>
+        <section>
+          <form id={`${compId}-form`}></form>
+          <div>
+            <input form={`${compId}-form`}></input>
+          </div>
+          <div>
+            <input></input>
+          </div>
+        </section>
       </article>
     </div>
   );
